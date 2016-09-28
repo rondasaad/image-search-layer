f294cc28d5ae9be8ae1adae87e188f8f275af1a8
RONDA Saad <rondasaad@gmail.com>
2016-09-28 01:09:37 +0000

    add search history

--- server.js
+++ server.js
@@ -1,30 +1,59 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
 
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
   key: 'AIzaSyBunbtKmh4Vd2dsx-PY6I4MDbOYu0DuFzs',
   cx: '011581325920002180380:b4n_v1lkgzu'
 });
- 
+
+var db_url =   'mongodb://user:pass@ds041556.mlab.com:41556/imagesearcher';
+
+app.get('/', function(req, res){
+    mongo.connect(db_url, function(err, db){
+        assert.equal(null, err);
+        var options = {
+            "limit": 10,
+            "skip": 0,
+            "sort": {when:-1},
+            "fields": {_id:0}
+        };
+        db.collection('history').find({}, options).toArray(function(err, docs ){
+            assert.equal(null, err);
+            res.writeHead(200,{'Content-Type': 'application/json'});
+            res.end(JSON.stringify(docs));
+            db.close();
+        });
+    });
+})
+
 app.get('/:q', function(req,res){ 
     var result = [];
-    googleSearch.build({
-      q: req.params.q,
-      num: 10,
-      start: req.query.offset
-     }, function(error, response) {
-        res.writeHead(200,{'Content-Type': 'application/json'});
-       for(var i in response.items)
+    var q = req.params.q;
+    googleSearch.build({q : q, num: 10, start: req.query.offset | 1},
+        function(error, response) {
+            res.writeHead(200,{'Content-Type': 'application/json'});
+            for(var i in response.items)
             result.push({
                 url: response.items[i].link,//pagemap.metatags[0]['og:url'],
                caption: response.items[i].title,//pagemap.metatags[0]['og:title'],
                image: response.items[i].pagemap.imageobject[0].url//metatags[0]['og:image'],
             });
-        
-        res.end(JSON.stringify(result));
-    });
-});
+            res.end(JSON.stringify(result));
 
+            mongo.connect(db_url, function(err, db){
+                assert.equal(null, err);
+                var when = new Date();
+                var when = when.toString();
+                db.collection('history').insertOne({term: q, when: when}, function(err, item){
+                    assert.equal(null, err);
+                    console.log(JSON.stringify(item));
+                    db.close();
+                });
+            });
+        });
+});
 
 app.listen(8080);
